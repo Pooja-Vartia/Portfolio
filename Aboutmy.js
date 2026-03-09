38e1d981f9b2427e0e7558beca641bacdb88f4c2
Pooja <poojavartia52@gmail.com>
2026-03-09 13:15:15 +0530

    feat: add Aboutmy component with basic container structure
    - Created Aboutmy functional component
    - Added container-fluid div for layout
    - Prepared structure for future content expansion.

--- Aboutmy.js
+++ Aboutmy.js
@@ -1,9 +1,8 @@
 import React from 'react'
-
 export default function Aboutmy() {
   return (
     <>
-    <div>
+    <div className='container-fluid'>
       <p>This is my about page....</p>
     </div>
     </>
